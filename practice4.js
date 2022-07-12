d53ef88d0a3994375f6161452c449f97796ced65
Lucas Barbosa <lucasbarbosa@outlook.com>
2022-07-12 21:59:54 +0100

    finishing practice

--- practice4.js
+++ practice4.js
@@ -2,6 +2,7 @@
 // (Use normalize data model)
 // 2. Create an interface that shows the total number of contacts by organization
 
+// e.g.
 // Company, Total
 // MoOngy, 100
 // Betacode, 10
@@ -9,6 +10,7 @@
 
 // 3. Create an interface that list all contact numbers of one organization
 
+// e.g.
 // Company, Numbers
 // MoOngy, ["+351 54634566345", "+351 455325324", ...]
 
@@ -16,7 +18,7 @@
 // 2. Training operation $group
 // 2.1 Training $sum
 // 2.2 Training $push or $addToSet
-// 3. Training $lokup
+// 3. Training $lookup
 
 // 4. Create an interface that list all contacts with organization info
 
@@ -28,35 +30,150 @@ const client = new MongoClient(url);
 client.connect();
 
 const db = client.db("contacts");
-const business = db.collection("business");
+const businessCollection = db.collection("business");
 const companiesCollection = db.collection("companies");
 
-// List all contacts paginated and ordered
 const insertContact = async function () {
   const companies = await companiesCollection.find().toArray();
-  console.log({ companies });
 
-  const answers = await inquirer.prompt([
+  const answer = await inquirer.prompt([
     { type: "input", name: "name", message: "Contact name: " },
+    { type: "input", name: "age", message: "Contact age: " },
     { type: "input", name: "number", message: "Contact number: " },
     {
       type: "rawlist",
-      name: "company",
+      name: "companyName",
       message: "Choose a company",
       choices: companies.map((company) => company.name)
     }
   ]);
 
-  business
+  businessCollection
     .insertOne({
-      name: answers.name,
-      number: answers.number,
-      company: answers.company
+      name: answer.name,
+      age: Number(answer.age),
+      number: answer.number,
+      company: companies.find((company) => company.name === answer.companyName)
+        ._id
     })
     .then((result) => console.log(result))
     .catch((error) => console.error(error));
 
-  console.log({ result });
+  menu();
+};
+
+const getTotalContactsByCompany = async function () {
+  const totalContactsByCompany = await businessCollection
+    .aggregate([
+      {
+        $group: {
+          _id: "$company",
+          total: {
+            $sum: 1
+          }
+        }
+      },
+      {
+        $lookup: {
+          from: "companies",
+          localField: "_id",
+          foreignField: "_id",
+          as: "companyInfo"
+        }
+      },
+      {
+        $unwind: {
+          path: "$companyInfo",
+          includeArrayIndex: "length",
+          preserveNullAndEmptyArrays: false
+        }
+      },
+      {
+        $project: {
+          companyName: "$companyInfo.name",
+          total: true,
+          _id: false
+        }
+      }
+    ])
+    .toArray();
+
+  console.table(totalContactsByCompany);
+
+  menu();
+};
+
+const getAllContactsWithCompanyInfo = async function () {
+  const allContacts = await businessCollection
+    .aggregate([
+      {
+        $lookup: {
+          from: "companies",
+          localField: "company",
+          foreignField: "_id",
+          as: "companyInfo"
+        }
+      },
+      {
+        $unwind: {
+          path: "$companyInfo",
+          includeArrayIndex: "length",
+          preserveNullAndEmptyArrays: false
+        }
+      }
+    ])
+    .toArray();
+
+  console.log(allContacts);
+
+  menu();
+};
+
+const getAllPhoneNumbersByCompany = async function () {
+  const companies = await companiesCollection.find().toArray();
+
+  const answers = await inquirer.prompt([
+    {
+      type: "rawlist",
+      name: "companyName",
+      message: "Choose a company: ",
+      choices: companies.map((company) => company.name)
+    }
+  ]);
+
+  const allPhoneNumbersByCompany = await businessCollection
+    .aggregate([
+      {
+        $lookup: {
+          from: "companies",
+          localField: "company",
+          foreignField: "_id",
+          as: "companyInfo"
+        }
+      },
+      {
+        $unwind: {
+          path: "$companyInfo",
+          includeArrayIndex: "length",
+          preserveNullAndEmptyArrays: false
+        }
+      },
+      {
+        $group: {
+          _id: "$companyInfo.name",
+          phoneNumbers: {
+            $push: "$number"
+          }
+        }
+      }
+    ])
+    .toArray();
+
+  const phoneNumbersByCompany = allPhoneNumbersByCompany.find(
+    (phoneNumbersByCompany) => phoneNumbersByCompany._id === answers.companyName
+  ).phoneNumbers;
+
+  console.log(answers.companyName, phoneNumbersByCompany);
 
   menu();
 };
@@ -69,9 +186,9 @@ const menu = async function () {
       message: "Choose an action: ",
       choices: [
         "Insert business contact",
-        "List business contacts",
-        "Get total contacts",
-        "Get all contacts by company",
+        "Get total contacts by company",
+        "Get all phone numbers by company",
+        "Get all contacts with company info",
         "Exit"
       ]
     }
@@ -81,14 +198,14 @@ const menu = async function () {
     case "Insert business contact":
       insertContact();
       break;
-    case "List business contacts":
-      listBusinessContacts();
+    case "Get total contacts by company":
+      getTotalContactsByCompany();
       break;
-    case "Get total contacts":
-      // getTotalContacts();
+    case "Get all phone numbers by company":
+      getAllPhoneNumbersByCompany();
       break;
-    case "Get all contacts by company":
-      // getAllContactsByCompany();
+    case "Get all contacts with company info":
+      getAllContactsWithCompanyInfo();
       break;
     case "Exit":
       process.exit();
